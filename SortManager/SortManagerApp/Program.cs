a1999ca8fc793b5a83944737f9fe052ab09ae69d
JayTG <jtgriffith6@gmail.com>
2022-03-01 19:14:16 +0000

    User input text

--- SortManager/SortManagerApp/Program.cs
+++ SortManager/SortManagerApp/Program.cs
@@ -4,8 +4,17 @@ public class Program
 {
     public static void Main()
     {
-
+        Console.WriteLine("Choose an sorting algorithm.");
+        Console.WriteLine("[1] Bubble, [2] Merge");
+        Console.Write("Please input a number: ");
+        int sortInput = Convert.ToInt32(Console.ReadLine());
+        //set sorting stuff here
+        Console.WriteLine("Type in the length of the array");
+        Console.Write("Please input a number: ");
+        int lenInput = Convert.ToInt32(Console.ReadLine());
+        //set array length here
     }
+
     public static int[] bubbleSort(int[] arr)
     {
         throw new NotImplementedException();
