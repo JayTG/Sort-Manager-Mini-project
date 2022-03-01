b8fe0927848f8aa9066104ad82c64bc5063fe825
Dmitry <dima.haswell@gmail.com>
2022-03-01 19:11:28 +0000

    gfhlsdgfdsiu

--- SortManager/SortManagerApp/Program.cs
+++ SortManager/SortManagerApp/Program.cs
@@ -1,2 +1,43 @@
+<<<<<<< HEAD
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+=======
+﻿namespace SortManagerApp;
+
+public class Program
+{
+    public static void Main()
+    {
+
+    }
+    public static int[] bubbleSort(int[] arr)
+    {
+        throw new NotImplementedException();
+    }
+    public static int[] mergeSort(int[] arr)
+    {
+        throw new NotImplementedException();
+    }
+    public static int[] heapSort(int[] arr)
+    {
+        throw new NotImplementedException();
+    }
+    public static int[] selectionSort(int[] arr)
+    {
+        throw new NotImplementedException();
+    }
+    public static int[] insertionSort(int[] arr)
+    {
+        throw new NotImplementedException();
+    }
+    public static int[] shellSort(int[] arr)
+    {
+        throw new NotImplementedException();
+    }
+    public static int[] quickSort(int[] arr)
+    {
+        throw new NotImplementedException();
+    }
+
+}
+>>>>>>> main
