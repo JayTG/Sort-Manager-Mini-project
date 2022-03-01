f8777d86f24d8de80866af640c900056bc6e1ff4
BoroBongo <43158992+BoroBongo@users.noreply.github.com>
2022-03-01 19:36:07 +0000

    Added BubbleSort and MergeSort methods

--- SortManager/SortManagerApp/Program.cs
+++ SortManager/SortManagerApp/Program.cs
@@ -15,6 +15,19 @@ public class Program
         Console.Write("Please input a number: ");
         int lenInput = Convert.ToInt32(Console.ReadLine());
         //set array length here
+
+        //Adam testing space
+        int[] test = { 4,5,2,7,3}; // {2,3,4,5,7}
+        int[] sortedArray = SortingAlgorithms.MergeArrays(test);
+        foreach(var i in test)
+        {
+            Console.Write($"{i}, ");
+        }
+        foreach (var i in sortedArray)
+        {
+            Console.Write($"{i}, ");
+        }
+        //EndOfAdamTestingSpace
     }
 
     public static int[] bubbleSort(int[] arr)
--- SortManager/SortManagerApp/SortingAlgorithms.cs
+++ SortManager/SortManagerApp/SortingAlgorithms.cs
@@ -34,25 +34,90 @@ namespace SortManagerApp
             }
             while (sorting);
         }
-        public static int[] MergeArrays(int[] array1, int[] array2)
+        public static int[] MergeArrays(int[] array)
         {
-            int[] temp = new int[array1.Length + array2.Length];
-            if (array1[array1.Length - 1] < array2[array2.Length - 1])
-            {
-                array1.CopyTo(temp, 0);
-                array2.CopyTo(temp, array1.Length);
-            }
-            else if (array2[array2.Length - 1] < array1[array1.Length - 1])
-            {
-                array2.CopyTo(temp, 0);
-                array1.CopyTo(temp, array2.Length);
-            }
+            int[] left;
+            int[] right;
+            int[] result = new int[array.Length];
+            //As this is a recursive algorithm, we need to have a base case to 
+            //avoid an infinite recursion and therfore a stackoverflow
+            if (array.Length <= 1)
+                return array;
+            // The exact midpoint of our array  
+            int midPoint = array.Length / 2;
+            //Will represent our 'left' array
+            left = new int[midPoint];
+
+            //if array has an even number of elements, the left and right array will have the same number of 
+            //elements
+            if (array.Length % 2 == 0)
+                right = new int[midPoint];
+            //if array has an odd number of elements, the right array will have one more element than left
             else
+                right = new int[midPoint + 1];
+            //populate left array
+            for (int i = 0; i < midPoint; i++)
+                left[i] = array[i];
+            //populate right array   
+            int x = 0;
+            //We start our index from the midpoint, as we have already populated the left array from 0 to midpoint
+            for (int i = midPoint; i < array.Length; i++)
             {
-                throw new Exception("Arrays are not sorted!");
+                right[x] = array[i];
+                x++;
             }
-            Array.Sort(temp);
-            return temp;
+            //Recursively sort the left array
+            left = MergeArrays(left);
+            //Recursively sort the right array
+            right = MergeArrays(right);
+            //Merge our two sorted arrays
+            result = Merge(left, right);
+            return result;
+        }
+
+        private static int[] Merge(int[] left, int[] right)
+        {
+            int resultLength = right.Length + left.Length;
+            int[] result = new int[resultLength];
+            //
+            int indexLeft = 0, indexRight = 0, indexResult = 0;
+            //while either array still has an element
+            while (indexLeft < left.Length || indexRight < right.Length)
+            {
+                //if both arrays have elements  
+                if (indexLeft < left.Length && indexRight < right.Length)
+                {
+                    //If item on left array is less than item on right array, add that item to the result array 
+                    if (left[indexLeft] <= right[indexRight])
+                    {
+                        result[indexResult] = left[indexLeft];
+                        indexLeft++;
+                        indexResult++;
+                    }
+                    // else the item in the right array wll be added to the results array
+                    else
+                    {
+                        result[indexResult] = right[indexRight];
+                        indexRight++;
+                        indexResult++;
+                    }
+                }
+                //if only the left array still has elements, add all its items to the results array
+                else if (indexLeft < left.Length)
+                {
+                    result[indexResult] = left[indexLeft];
+                    indexLeft++;
+                    indexResult++;
+                }
+                //if only the right array still has elements, add all its items to the results array
+                else if (indexRight < right.Length)
+                {
+                    result[indexResult] = right[indexRight];
+                    indexRight++;
+                    indexResult++;
+                }
+            }
+            return result;
         }
 
 
