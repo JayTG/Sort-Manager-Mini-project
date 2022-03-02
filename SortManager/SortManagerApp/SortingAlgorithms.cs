51cce15dac2c27f6f616e7a3c738642807ed2c3d
BoroBongo <43158992+BoroBongo@users.noreply.github.com>
2022-03-02 12:00:20 +0000

    Added heapsort and changed main in accordance to it

--- SortManager/SortManagerApp/SortingAlgorithms.cs
+++ SortManager/SortManagerApp/SortingAlgorithms.cs
@@ -21,6 +21,9 @@ namespace SortManagerApp
                 case 2:
                     sortedArray = SortingAlgorithms.MergeArrays(arrayToSort);
                     break;
+                case 3:
+                    sortedArray = SortingAlgorithms.HeapSort(arrayToSort);
+                    break;
             }
             time.Subtract(DateTime.Now);
             return (sortedArray, time.Millisecond);
@@ -96,6 +99,40 @@ namespace SortManagerApp
             //Merge our two sorted arrays
             result = Merge(left, right);
             return result;
+        }
+        private static int[] HeapSort(int[] array)
+        {
+            int[] result = (int[])array.Clone();
+            int n = result.Length;
+            for (int i = n / 2 - 1; i >= 0; i--) 
+            {
+                Heapify(result, n, i); }
+            for (int i = n - 1; i >= 0; i--)
+            {
+                int temp = result[0];
+                result[0] = result[i];
+                result[i] = temp;
+                Heapify(result, i, 0);
+            }
+            return result;
+        }
+
+        private static void Heapify(int[] array, int n, int i)
+        {
+            int largest = i;
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+            if (left < n && array[left] > array[largest])
+                largest = left;
+            if (right < n && array[right] > array[largest])
+                largest = right;
+            if (largest != i)
+            {
+                int swap = array[i];
+                array[i] = array[largest];
+                array[largest] = swap;
+                Heapify(array, n, largest);
+            }
         }
 
         private static int[] Merge(int[] left, int[] right)
